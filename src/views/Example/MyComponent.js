a38c50bbf72d252e366d7408211191ac5458eb10
Nguyen Dinh Khoi Nguyen <hpkhoinguyen@gmail.com>
2025-08-21 15:29:54 +0700

    state in class component

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  // key: value
+  state = {
+    name: "Nguyen",
+    chanel: "Hoi Dan IT",
+  };
+  /* 
+    function can return <html></html> (template)
+    JSX: =>return block  
+  */
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
-    return <div>Hello my component</div>;
+    let name = "Eric";
+    // use code javascript in html
+    return (
+      // code block React.Fragment (short cut)
+      <>
+        <div className="first">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => {
+              this.handleOnChangeName(event);
+            }}
+          />{" "}
+          My name is {this.state.name}
+        </div>
+        <div className="second">My Youtube chanel is {this.state.chanel}</div>;
+      </>
+    );
   }
 }
 
