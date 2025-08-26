ab96379d060f9edfaf51cd25b32d9c95bf396bce
Nguyen Dinh Khoi Nguyen <hpkhoinguyen@gmail.com>
2025-08-26 10:25:54 +0700

    split component

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ChildComponent from "./ChildComponent";
+import AddComponent from "./AddComponent";
+
 class MyComponent extends React.Component {
   // key: value
   state = {
-    firstName: "",
-    lastName: "",
     arrJobs: [
       { id: "abcJob1", title: "Developers", salary: "500" },
       { id: "abcJob2", title: "Testers", salary: "290" },
@@ -16,20 +16,6 @@ class MyComponent extends React.Component {
     JSX: =>return block  
   */
 
-  handleChangeFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value,
-    });
-  };
-  handleChangeLastName = (event) => {
-    this.setState({
-      lastName: event.target.value,
-    });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(">>> check data input:", this.state);
-  };
   render() {
     // re-render
     console.log("call render:", this.state);
@@ -37,36 +23,9 @@ class MyComponent extends React.Component {
     return (
       // code block React.Fragment (short cut)
       <>
-        <form>
-          <label htmlFor="fname">First name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.firstName}
-            onChange={(event) => this.handleChangeFirstName(event)}
-          />
-          <br />
-          <label htmlFor="lname">Last name</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.lastName}
-            onChange={(event) => this.handleChangeLastName(event)}
-          />
-          <br />
-          <input
-            type="submit"
-            onClick={(event) => {
-              this.handleSubmit(event);
-            }}
-          />
-        </form>
-        <ChildComponent
-          name={this.state.firstName}
-          age={"22"}
-          address={"Ha Noi"}
-          arrJobs={this.state.arrJobs}
-        />
+        <AddComponent />
+
+        <ChildComponent arrJobs={this.state.arrJobs} />
         {/* props: property */}
       </>
     );
