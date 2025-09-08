97533eb1409f15700ed16dbb17cb64d0791891a8
Nguyen Dinh Khoi Nguyen <hpkhoinguyen@gmail.com>
2025-09-08 10:37:00 +0700

    setup Redux

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import logo from "../../assets/image/RobloxScreenShot20250704_234220148.png";
+
 class Home extends React.Component {
   componentDidMount() {
     // setTimeout(() => {
@@ -13,6 +15,17 @@ class Home extends React.Component {
       <>
         {console.log(">>> check props:", this.props)}
         <div>Hello World with Reactjs</div>
+        <div>
+          <img
+            src={logo}
+            style={{
+              width: "200px",
+              height: "200px",
+              marginTop: "20px",
+              objectFit: "cover",
+            }}
+          />
+        </div>
       </>
     );
   }
